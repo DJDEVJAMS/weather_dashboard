0005a104f5d2b4e2dd0fe294a2936081c2c128de
DJDEVJAMS <alex.art.brown@gmail.com>
2024-08-23 15:58:59 -0500

    reapply

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,4 @@ const cityInput = document.getElementById('cityInput');
 const currentWeatherData = document.getElementById('currentWeatherData');
 const forecastData = document.getElementById('forecastData');
 const historyList = document.getElementById('historyList');
+const apiKey = 'fb361e84ed523bb989390f84590d89c4';
