016c8d23c408c3fd29cba13c3fe436dee65fdc0d
DJDEVJAMS <alex.art.brown@gmail.com>
2024-08-23 15:22:17 -0500

    js set consts

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const apiKey = 'fb361e84ed523bb989390f84590d89c4'; //  API key
+const searchForm = document.getElementById('searchForm');
+const cityInput = document.getElementById('cityInput');
+const currentWeatherData = document.getElementById('currentWeatherData');
+const forecastData = document.getElementById('forecastData');
+const historyList = document.getElementById('historyList');
