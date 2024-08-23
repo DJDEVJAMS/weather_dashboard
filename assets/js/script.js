a60d07fe00c7d5de57b9b6ef70725674588c78d8
DJDEVJAMS <alex.art.brown@gmail.com>
2024-08-23 15:53:53 -0500

    remove api key for now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-const apiKey = 'fb361e84ed523bb989390f84590d89c4'; //  API key
 const searchForm = document.getElementById('searchForm');
 const cityInput = document.getElementById('cityInput');
 const currentWeatherData = document.getElementById('currentWeatherData');
